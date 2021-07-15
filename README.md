45f0b0c3a380e60edb6acadc2505769c9750842f
Cesar Castillo <cesar.castillo@leanix.net>
2021-07-15 14:57:50 +0200

    INTG-2357 Add upsert calls for Hub endpoint

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
-# LeanIX Integration API Default Config Action
+# LeanIX Integration Hub Connector Resgiser Action
 
-This action provides a standard way of provisioning a default configuration into the Integration-API of all regions.
+This action provides a standard way of registration a connector into the Integration-HUB of all regions.
 
 ## Usage
 
-This action reads the default configuration from a file and calls the Integration-API to provision the provided config.
+This action reads the connector definition from a file and calls the Integration-Hub to register the provided connector.
 Use in advance the provided [leanix/secrets-action](https://github.com/leanix/secrets-action) to inject the required secrets.
 
 A simple provision step in the connector's workflow would look like this:
 ```yaml
-- name: Provision default config
-  uses: leanix/integration-api-default-config-action@main
+- name: Register connector in Hub
+  uses: leanix/integration-hub-connector-register-action@main
   with:
     environment: 'prod'  
 ```
-This reads the file `integration-api-default-config.json` from the root of your repository and provisions it globally on all prod instances of Integration-API.
+This reads the file `integration-hub-connecotr-definition.json` from the root of your repository and register it globally on all prod instances of Integration-Hub.
 ### Input Parameter
 | input | required | default | description |
 |-------|----------|---------|-------------|
-|default_connector_file|no|`integration-api-default-config.json`|The location of the file that contains the default configuration that is used as the input for this action.|
+|default_connector_file|no|`integration-hub-connector-definition.json`|The location of the file that contains the definition of the connector that is used as the input for this action.|
 |environment|yes|test|The environment to provision to, e.g. test or prod|
 |region|no|-|The region to provision to, e.g. westeurope or australiaeast. Leave empty to provision to all regions.|
+
+For the complete structure of the `Connector Definition` file see  in  https://leanix.atlassian.net/wiki/spaces/FLOW/pages/4215773679/Getting+Started 
 
 ## Requires
 This action requires following GitHub actions in advance:
--- entrypoint.sh
+++ entrypoint.sh
@@ -18,8 +18,8 @@ CONNECTOR_NAME=$(cat ${NEW_CONNECTOR_FILE} | jq -r '.name')
 echo "Found provided config for connector ='${CONNECTOR_NAME}'"
 
 echo "Login to Azure ..."
-az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
-az account set -s $ARM_SUBSCRIPTION_ID
+echo "az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID"
+echo "az account set -s $ARM_SUBSCRIPTION_ID"
 
 if [[ -z "${INPUT_REGION}" ]]; then
   # use all regions
@@ -38,44 +38,71 @@ for REGION in $REGIONS; do
   if [[ "${REGION}" == "horizon" ]]; then
     # edge-case for horizon
     KEY_VAULT_NAME="lxeastusprod"
-    VAULT_SECRET_KEY="integration-api-horizon-oauth-secret-horizon-svc"
+    VAULT_SECRET_KEY="integration-hub-horizon-oauth-secret-horizon-svc"
     REGION_ID="app-9"
   else
     KEY_VAULT_NAME="lx${REGION}${INPUT_ENVIRONMENT}"
-    VAULT_SECRET_KEY="integration-api-oauth-secret-${REGION_ID}-svc"
+    VAULT_SECRET_KEY="integration-hub-oauth-secret-${REGION_ID}-svc"
   fi
   # hard-coded test environment
   if [[ "${INPUT_ENVIRONMENT}" == "test" ]]; then
     KEY_VAULT_NAME="lxwesteuropetest"
-    VAULT_SECRET_KEY="integration-api-oauth-secret-test-svc-flow-2"
+    VAULT_SECRET_KEY="integration-hub-oauth-secret-test-svc-flow-2"
     REGION_ID="test-app-flow-2"
   fi
 
   echo "Using key '${VAULT_SECRET_KEY}' to fetch the SYSTEM user secret from Azure Key Vault '${KEY_VAULT_NAME}' ..."
-  VAULT_SECRET_VALUE=$(az keyvault secret show --vault-name ${KEY_VAULT_NAME} --name ${VAULT_SECRET_KEY} | jq -r .value)
+  #VAULT_SECRET_VALUE=$(az keyvault secret show --vault-name ${KEY_VAULT_NAME} --name ${VAULT_SECRET_KEY} | jq -r .value)
+  VAULT_SECRET_VALUE=$MTM_CLIENT_SECRET
 
   echo "Fetching oauth token from ${REGION_ID}.leanix.net ..."
   TOKEN=$(curl --silent --request POST \
     --url "https://${REGION_ID}.leanix.net/services/mtm/v1/oauth2/token" \
     --header 'content-type: application/x-www-form-urlencoded' \
-    --header 'User-Agent: integration-api-default-config-action' \
-    --data client_id=integration-api \
+    --header 'User-Agent: integration-hub-connector-register-action' \
+    --data client_id=integration-hub \
     --data client_secret=${VAULT_SECRET_VALUE} \
     --data grant_type=client_credentials \
     | jq -r .'access_token')
 
-  echo "GET integration-api/v1/defaultConfigurations/${CONNECTOR_NAME} ..."
-  REMOTE_CONFIG_STATUS_CODE=$(curl --write-out %{http_code} --silent --output /dev/null --request GET \
-    --url "https://${REGION_ID}.leanix.net/services/integration-api/v1/defaultConfigurations/${CONNECTOR_NAME}" \
+  echo "GET integration-hub/v1/connectorTemplates/name/${CONNECTOR_NAME} ..."
+  CONNECTOR_ID=$(curl --silent --request GET \
+    --url "https://${REGION_ID}.leanix.net/services/integration-hub/v1/connectorTemplates/name/${CONNECTOR_NAME}" \
     --header "Authorization: Bearer ${TOKEN}" \
-    --header 'User-Agent: integration-api-default-config-action' \
-    --header 'Accept: application/json')
+    --header 'User-Agent: integration-hub-connector-register-action' \
+    --header 'Accept: application/json' \
+    | jq -r .'id')
   
-  if [[ "${REMOTE_CONFIG_STATUS_CODE}" -eq 200 ]] ; then
-    echo "Found remote config by externalId='${CONNECTOR_NAME}'"
-    #TODO: PUT new config
+  if [[ "${CONNECTOR_ID}" != "null" ]] ; then
+    echo "Found connector Id: '${CONNECTOR_ID}'  for connector name='${CONNECTOR_NAME}'"
+    UPSERT_RESULT=$(curl --request PUT --write-out %{http_code} --silent --output /dev/null \
+    --url "https://${REGION_ID}.leanix.net/services/integration-hub/v1/connectorTemplates/${CONNECTOR_ID}" \
+    --header "Authorization: Bearer ${TOKEN}" \
+    --header "Content-Type: application/json" \
+    --header 'User-Agent: integration-hub-connector-register-action' \
+    --header 'Accept: application/json' \
+    --data-binary @${NEW_CONNECTOR_FILE})
+
+    if [[ "${UPSERT_RESULT}" -eq 200 ]] ; then
+      echo "Successfully modified connector ${CONNECTOR_NAME}"
+    else
+      echo "Failed to update connector Id: ${CONNECTOR_ID} . Http error code: ${UPSERT_RESULT}"
+    fi
   else
-    echo "No remote config found by externalId='${CONNECTOR_NAME}'"
-    #TODO: POST new config
+    echo "No remote connector found with the name='${CONNECTOR_NAME} ' . Creating a new connector"
+    CREATE_RESULT=$(curl --request POST --write-out %{http_code} --silent --output /dev/null \
+    --url "https://${REGION_ID}.leanix.net/services/integration-hub/v1/connectorTemplates" \
+    --header "Authorization: Bearer ${TOKEN}" \
+    --header "Content-Type: application/json" \
+    --header 'User-Agent: integration-hub-connector-register-action' \
+    --header 'Accept: application/json' \
+    --data-binary @${NEW_CONNECTOR_FILE} \
+    | jq -r '.id')
+
+    if [[ "${UPSERT_RESULT}" -eq 200 ]] ; then
+      echo "Successfully created a new connector,  Id: ${CONNECTOR_ID}"
+    else
+      echo "Failed to create new connector. Http error code: ${CREATE_RESULT}"
+    fi
   fi
 done
